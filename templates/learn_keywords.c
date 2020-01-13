c878e6ae7bbc37a077da76732e171e816a6c132d
Cinnamon <lukaszkomsta@yahoo.com>
2020-01-13 10:11:36 +0000

    learn_keywords.c done

---
+++ templates/learn_keywords.c
@@ -0,0 +1,45 @@
+//Source: https://www.programiz.com/c-programming/list-all-keywords-c-language
+#include <stdio.h>
+int main()
+{
+//auto
+//break
+//case
+//char
+//const
+//continue
+//default
+//do
+//double
+//else
+//enum
+//extern
+//float
+//for
+//goto
+//if
+//int
+//long
+//register
+//return
+//short
+//signed
+//sizeof
+//static
+//struct
+//switch
+//typedef
+//union
+//unsigned
+//void
+//volatile
+//while
+
+//For example:
+//auto
+//The auto keyword declares automatic variables.
+auto int var1;//This statement suggests that var1 is a variable of storage class auto and type int.
+//Variables declared within function bodies are automatic by default.
+//They are recreated each time a function is executed.
+
+}
