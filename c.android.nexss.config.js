662836b7a25f89a09daaafd5adbe76cf268245e8
Marcin Polak <mapoart@gmail.com>
2023-05-07 14:29:44 +0200

    Support for Android platforms

---
+++ c.android.nexss.config.js
@@ -0,0 +1,22 @@
+let languageConfig = Object.assign({}, require("./c.win32.nexss.config"));
+
+languageConfig.builders = {
+  gcc: {
+    install: "pkg install -y gcc",
+    //build: "pkg --output <destinationFile> --out-path <destinationPath> <file>",
+    command: "gcc",
+    build: "gcc",
+    args: `-std=c11 -o <destinationFile> <file> && <destinationFile>`,
+    help: ``,
+  },
+  llvm: {
+    install: "pkg install -y llvm",
+    //build: "pkg --output <destinationFile> --out-path <destinationPath> <file>",
+    command: "clang",
+    build: "clang",
+    args: `-std=c89 -o <destinationFile> <file>`,
+    help: ``,
+  },
+};
+
+module.exports = languageConfig;
